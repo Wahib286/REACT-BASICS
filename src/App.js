690e9f53fafefc6a1f5da76bab8da55767ef6aa5
Wahib <wahibbashar.it25@jecrc.ac.in>
2025-03-20 03:23:32 +0530

    setting up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import "./App.css";
+import React from 'react';
+import NewProduct from './components/NewProduct';
 
-function App() {
+import Products from './components/Products';
+
+const App = () => {
+  const products = [
+    {
+      id: 'p1',
+      title: 'Nirma',
+      amount: 100,
+      date: new Date(2021, 8, 10),
+    },
+    { 
+      id: 'p2', 
+      title: 'Sirf Excel', 
+      amount: 200, 
+      date: new Date(2021, 2, 2) },
+    {
+      id: 'p3',
+      title: 'Tide',
+      amount: 130,
+      date: new Date(2021, 12, 28),
+    },
+    {
+      id: 'p4',
+      title: 'Maggi',
+      amount: 450,
+      date: new Date(2021, 5, 5),
+    },
+  ];
+
+  function printProductData(data) {
+    console.log("i am inside APP.js")
+    console.log(data)
+  }
+
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+    <div>
+      <NewProduct pranay = {printProductData} />
+      <Products items={products} />
+    </div>
   );
 }
 
